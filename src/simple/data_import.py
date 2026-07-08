cf5e88473c58aebac943a497bbbf54a79a1cbbb4
AnEdelweiss <edelweiss.courriel@proton.me>
2026-07-08 11:04:55 +0300

    changement prov et prov used

--- src/simple/data_import.py
+++ src/simple/data_import.py
@@ -460,8 +460,8 @@ def create_data(document_data,document_miappe,login,wd_experience,silex_API_Clie
                                             metadata = {"Round Order": row["Round Order"]},
                                             provenance = silex.DataProvenanceModel(
                                                 uri = prov_dict[prov],
-                                                prov_used = [Prov_Used],
-                                                prov_was_associated_with = [Prov_Was_Associated_With],
+                                                prov_used = [Prov_Used] if Prov_Used else [],
+                                                prov_was_associated_with = Prov_Was_Associated_With,
                                                 settings = Setting_Dict,
                                                 experiments = [NameExp_uri[NameExp]]))
                     bodies.append(body)
