a7eab362f01944271903b17dd1562727d78f6bb8
AnEdelweiss <edelweiss.courriel@proton.me>
2026-08-04 14:51:59 +0300

    small fixes

--- src/simple/__main__.py
+++ src/simple/__main__.py
@@ -1,4 +1,24 @@
-from simple.cli import main
+import argparse
+
+from simple.cli import main as cli_main
+from simple.gui import main as gui_main
+
+
+def main():
+    parser = argparse.ArgumentParser(description="SIMPLE - Scientific data import tool")
+    parser.add_argument(
+        "--gui", 
+        action="store_true", 
+        help="Launch the graphical user interface"
+    )
+    
+    # Parse known args in case uv passes extra arguments
+    args, unknown = parser.parse_known_args()
+
+    if args.gui:
+        gui_main()
+    else:
+        cli_main()
 
 if __name__ == "__main__":
     main()
